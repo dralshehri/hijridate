61ae735bba3db02d582b45f02fd56e9bb07f22f0
dralshehri <dralshehri@users.noreply.github.com>
2018-11-14 01:03:46 +0300

    improve documentation

--- src/hijriconverter/convert.py
+++ src/hijriconverter/convert.py
@@ -1,22 +1,28 @@
 from hijriconverter import helper, ummalqura
 from typing import Tuple
-import datetime
+from datetime import date
 import bisect
 
 
 class Hijri:
-    """Hijri(year, month, day, calendar)
-    A Hijri object represents a Hijri date (year, month and day) in lunar
+    """A Hijri object represents a Hijri date (year, month and day) in lunar
     or solar Hijri calendar.
     """
 
     def __init__(self, year: int, month: int, day: int,
                  calendar: str = 'lunar') -> None:
-        """Construct Hijri object.
-        The year, month and day arguments are required and must be integers.
-        The calendar argument is optional and must be a string. It may be
-        'lunar' or 'solar'. Default is 'lunar'.
         """
+        :param year: Hijri year
+        :type year: int
+        :param month: Hijri month
+        :type month: int
+        :param day: Hijri day
+        :type day: int
+        :param calendar: Hijri calendar which may be ``lunar`` or ``solar``
+            (default is ``lunar``)
+        :type calendar: str
+        """
+
         calendar = helper.check_hijri_calendar(calendar)
         year, month, day = helper.check_date(year, month, day, calendar)
         self._year = year
@@ -25,113 +31,131 @@ class Hijri:
         self._calendar = calendar
 
     def __repr__(self) -> str:
-        """Return a string representation of the Hijri object."""
         class_name = self.__class__.__name__
         return '{}({}, {}, {}, {})'.format(class_name,
                                            self._year, self._month, self._day,
                                            self._calendar)
 
-    def isoformat(self) -> str:
-        """Return a string representing the date in ISO format ‘YYYY-MM-DD’."""
-        return '{:04}-{:02}-{:02}'.format(self._year, self._month, self._day)
-
-    __str__ = isoformat
+    def __str__(self) -> str:
+        return self.isoformat()
 
     @property
     def year(self) -> int:
-        """Return year as an integer"""
+        """Return Hijri year as an integer."""
         return self._year
 
     @property
     def month(self) -> int:
-        """Return month as an integer"""
+        """Return Hijri month as an integer."""
         return self._month
 
     @property
     def day(self) -> int:
-        """Return day as an integer"""
+        """Return Hijri day as an integer."""
         return self._day
 
     def datetuple(self) -> Tuple[int, int, int]:
-        """Return date as a tuple of (year, month, day)."""
+        """Return Hijri date as a tuple of (year, month, day)."""
         return self._year, self._month, self._day
 
+    def isoformat(self) -> str:
+        """Return Hijri date in ISO format 'YYYY-MM-DD'."""
+        return '{:04}-{:02}-{:02}'.format(self._year, self._month, self._day)
+
     def slashformat(self) -> str:
-        """Return a string representing the date in slash format
-        ‘DD/MM/YYYY’.
-        """
+        """Return Hijri date in slash format 'DD/MM/YYYY'."""
         return '{:02}/{:02}/{:04}'.format(self._day, self._month, self._year)
 
     def month_days(self) -> int:
-        """Return number of days in month as an integer."""
+        """Return number of days in Hijri month."""
         return helper.hijri_month_days(self._year, self._month, self._calendar)
 
     def month_name(self, language: str = 'en') -> str:
-        """Return month name as a string in specified language.
-        The language argument is optional and must be a string. It may
-        be 'en' for English or 'ar' for Arabic. Default is 'en'.
+        """Return Hijri month name.
+
+        :param language: language which may be 'en' or 'ar'
+            (default is ``en``)
+        :type language: str
         """
-        return ummalqura.month_names[self._calendar][language][self._month]
+
+        return ummalqura.months[language][self._calendar][self._month]
 
     def weekday(self) -> int:
-        """Return day of week as an integer, where Mon is 0 and Sun is 6."""
+        """Return day of week, where Monday is 0 ... Sunday is 6."""
         jd = helper.hijri_to_julian(self._year, self._month, self._day,
                                     self._calendar)
         return int(jd % 7)
 
     def isoweekday(self) -> int:
-        """Return day of week as an integer, where Mon is 1 and Sun is 7."""
+        """Return day of week, where Monday is 1 ... Sunday is 7."""
         jd = helper.hijri_to_julian(self._year, self._month, self._day,
                                     self._calendar)
         return int(jd % 7) + 1
 
     def day_name(self, language: str = 'en') -> str:
-        """Return day name as a string in specified language.
-        The language argument is optional and must be a string. It may
-        be 'en' for English or 'ar' for Arabic. Default is 'en'.
-        """
-        return ummalqura.day_names[language][self.weekday()]
+        """Return day name.
 
-    def to_gregorian(self) -> datetime.date:
-        """Return a converted gregorian date as a datetime.date object."""
+        :param language: language which may be ``en`` or ``ar``
+            (default is 'en')
+        :type language: str
+        """
+
+        return ummalqura.days[language][self.weekday()]
+
+    def to_gregorian(self) -> date:
+        """Convert Hijri date to Gregorian date.
+
+        :return: Gregorian date object
+        :rtype: datetime.date
+        """
+
         jd = helper.hijri_to_julian(self._year, self._month, self._day,
                                     self._calendar)
         gregorian = helper.julian_to_gregorian(jd)
-        return datetime.date(*gregorian)
+        return date(*gregorian)
 
 
 class Gregorian:
-    """Gregorian(year, month, day)
-    A Gregorian object represents a Gregorian date (year, month and day) in
+    """A Gregorian object represents a Gregorian date (year, month and day) in
     Gregorian calendar.
     """
 
     def __init__(self, year: int, month: int, day: int) -> None:
-        """Construct Gregorian object.
-        The year, month and day arguments are required and must be integers.
         """
+        :param year: Gregorian year
+        :type year: int
+        :param month: Gregorian month
+        :type month: int
+        :param day: Gregorian day
+        :type day: int
+        """
+
         year, month, day = helper.check_date(year, month, day, 'gregorian')
         self._year = year
         self._month = month
         self._day = day
 
     def __repr__(self) -> str:
-        """Return a string representation of the Gregorian object."""
         class_name = self.__class__.__name__
         return '{}({}, {}, {})'.format(class_name,
                                        self._year, self._month, self._day)
 
     def to_hijri(self, calendar: str = 'lunar') -> Hijri:
-        """Return a converted hijri date as a Hijri object.
-        The calendar argument is optional and must be a string. It may
-        be 'lunar' or 'solar'. Default is 'lunar'.
+        """Convert Gregorian date to Hijri date.
+
+        :param calendar: Hijri calendar which may be ``lunar`` or ``solar``
+            (default is ``lunar``)
+        :type calendar: str
+        :return: Hijri date object
+        :rtype: Hijri
         """
-        helper.check_hijri_calendar(calendar)
+
+        calendar = helper.check_hijri_calendar(calendar)
         jd = helper.gregorian_to_julian(self._year, self._month, self._day)
         mjd = helper.julian_to_modified_julian(jd)
-        month_starts = ummalqura.month_starts[calendar]
+        month_starts = ummalqura.starts[calendar]
         i = bisect.bisect_right(month_starts, mjd)
-        months = i + ummalqura.first_month_offset[calendar]
+        months = i + ummalqura.offset[calendar]
         years = int((months - 1) / 12)
         year = years + 1
         month = months - 12 * years
