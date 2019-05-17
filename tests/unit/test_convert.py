e5db85d814d96b0e0808150af838ae819c085472
Mohammed H Alshehri <542855+mhalshehri@users.noreply.github.com>
2019-05-17 03:31:58 +0300

    update unit tests

--- tests/unit/test_convert.py
+++ tests/unit/test_convert.py
@@ -1,15 +1,11 @@
 import pytest
-from hijriconverter import convert, ummalqura
+from datetime import date
+from hijriconverter import convert
 
 
 @pytest.fixture(scope="module")
-def hijri_lunar():
+def hijri():
     return convert.Hijri(1410, 8, 13)
-
-
-@pytest.fixture(scope="module")
-def hijri_solar():
-    return convert.Hijri(1368, 6, 19, "solar")
 
 
 @pytest.fixture(scope="module")
@@ -17,106 +13,138 @@ def gregorian():
     return convert.Gregorian(1990, 3, 10)
 
 
-def test_hijri_representation(hijri_lunar):
-    assert hijri_lunar.__repr__() == "Hijri(1410, 8, 13, lunar)"
+def test_hijri_representation(hijri):
+    assert hijri.__repr__() == "Hijri(1410, 8, 13)"
 
 
-def test_hijri_string_representation(hijri_lunar):
-    assert hijri_lunar.__str__() == "1410-08-13"
+def test_hijri_string_representation(hijri):
+    assert hijri.__str__() == "1410-08-13"
 
 
 @pytest.mark.parametrize(
     "test_input", ["__eq__", "__gt__", "__ge__", "__lt__", "__le__"]
 )
-def test_test_hijri_comparison_exception(hijri_lunar, test_input):
+def test_test_hijri_comparison_exception(hijri, test_input):
     with pytest.raises(TypeError) as e:
-        getattr(hijri_lunar, test_input)("w")
+        getattr(hijri, test_input)("w")
     assert str(e.value) == "second operand must be 'Hijri' object"
 
 
-def test_hijri_equality(hijri_lunar):
-    assert hijri_lunar == convert.Hijri(1410, 8, 13)
-    assert hijri_lunar != convert.Hijri(1410, 8, 14)
+def test_hijri_equality(hijri):
+    assert hijri == convert.Hijri(1410, 8, 13)
+    assert hijri != convert.Hijri(1410, 8, 14)
 
 
-def test_hijri_ordering(hijri_lunar):
-    assert hijri_lunar > convert.Hijri(1410, 8, 12)
-    assert hijri_lunar >= convert.Hijri(1410, 8, 13)
-    assert hijri_lunar < convert.Hijri(1410, 8, 14)
-    assert hijri_lunar <= convert.Hijri(1410, 8, 13)
+def test_hijri_ordering(hijri):
+    assert hijri > convert.Hijri(1410, 8, 12)
+    assert hijri >= convert.Hijri(1410, 8, 13)
+    assert hijri < convert.Hijri(1410, 8, 14)
+    assert hijri <= convert.Hijri(1410, 8, 13)
 
 
-def test_hijri_fromisoformat(hijri_lunar):
-    assert convert.Hijri.fromisoformat("1410-08-13") == hijri_lunar
+def test_hijri_fromisoformat(hijri):
+    assert convert.Hijri.fromisoformat("1410-08-13") == hijri
 
 
-def test_hijri_year(hijri_lunar, hijri_solar):
-    assert hijri_lunar.year == 1410
-    assert hijri_solar.year == 1368
+def test_hijri_year(hijri):
+    assert hijri.year == 1410
 
 
-def test_hijri_month(hijri_lunar):
-    assert hijri_lunar.month == 8
+def test_hijri_month(hijri):
+    assert hijri.month == 8
 
 
-def test_hijri_day(hijri_lunar):
-    assert hijri_lunar.day == 13
+def test_hijri_day(hijri):
+    assert hijri.day == 13
 
 
-def test_hijri_datetuple(hijri_lunar):
-    assert hijri_lunar.datetuple() == (1410, 8, 13)
+def test_hijri_datetuple(hijri):
+    assert hijri.datetuple() == (1410, 8, 13)
 
 
-def test_hijri_isoformat(hijri_lunar):
-    assert hijri_lunar.isoformat() == "1410-08-13"
+def test_hijri_isoformat(hijri):
+    assert hijri.isoformat() == "1410-08-13"
 
 
-def test_hijri_slashformat(hijri_lunar):
-    assert hijri_lunar.slashformat() == "13/08/1410"
+def test_hijri_slashformat(hijri):
+    assert hijri.slashformat() == "13/08/1410"
 
 
-def test_hijri_month_days(hijri_lunar, hijri_solar):
-    assert hijri_lunar.month_days() == 29
-    assert hijri_solar.month_days() == 29
+def test_hijri_month_name(hijri):
+    assert hijri.month_name() == "Sha’ban"
+    assert hijri.month_name("ar") == "شعبان"
 
 
-def test_hijri_month_name(hijri_lunar, hijri_solar):
-    assert hijri_lunar.month_name() == "Sha’ban"
-    assert hijri_lunar.month_name("ar") == "شعبان"
-    assert hijri_solar.month_name() == "Pisces"
-    assert hijri_solar.month_name("ar") == "الحوت"
+def test_hijri_weekday(hijri):
+    assert hijri.weekday() == 5
 
 
-def test_hijri_weekday(hijri_lunar):
-    assert hijri_lunar.weekday() == 5
+def test_hijri_iso_weekday(hijri):
+    assert hijri.isoweekday() == 6
 
 
-def test_hijri_iso_weekday(hijri_lunar):
-    assert hijri_lunar.isoweekday() == 6
+def test_hijri_day_name(hijri):
+    assert hijri.day_name() == "Saturday"
+    assert hijri.day_name("ar") == "السبت"
 
 
-def test_hijri_day_name(hijri_lunar):
-    assert hijri_lunar.day_name() == "Saturday"
-    assert hijri_lunar.day_name("ar") == "السبت"
+def test_hijri_notation(hijri):
+    assert hijri.notation() == "AH"
+    assert hijri.notation("ar") == "هـ"
 
 
-def test_hijri_to_julian(hijri_lunar, hijri_solar):
-    assert hijri_lunar.to_julian() == 2447961
-    assert hijri_solar.to_julian() == 2447961
+def test_hijri_to_julian(hijri):
+    assert hijri.to_julian() == 2447961
 
 
-def test_hijri_to_gregorian(hijri_lunar, hijri_solar):
-    assert hijri_lunar.to_gregorian().timetuple()[:3] == (1990, 3, 10)
-    assert hijri_solar.to_gregorian().timetuple()[:3] == (1990, 3, 10)
+def test_hijri_to_gregorian(hijri):
+    assert hijri.to_gregorian().datetuple() == (1990, 3, 10)
+
+
+def test_gregorian_fromdate():
+    test_date = date(2014, 12, 28)
+    assert convert.Gregorian.fromdate(test_date).datetuple() == (2014, 12, 28)
+
+
+def test_gregorian_datetuple(gregorian):
+    assert gregorian.datetuple() == (1990, 3, 10)
+
+
+def test_gregorian_month_name(gregorian):
+    assert gregorian.month_name() == "March"
+    assert gregorian.month_name("ar") == "مارس"
+
+
+def test_gregorian_day_name(gregorian):
+    assert gregorian.day_name() == "Saturday"
+    assert gregorian.day_name("ar") == "السبت"
+
+
+def test_gregorian_notation(gregorian):
+    assert gregorian.notation() == "CE"
+    assert gregorian.notation("ar") == "م"
+
+
+def test_gregorian_to_julian(gregorian):
+    assert gregorian.to_julian() == 2447961
 
 
 def test_gregorian_to_hijri(gregorian):
     assert gregorian.to_hijri().datetuple() == (1410, 8, 13)
 
 
-def test_gregorian_valid_range(gregorian):
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (1990, 3, 10),
+        (1937, 3, 14),
+        (2077, 11, 16),
+    ],
+)
+def test_gregorian_valid_range(test_input):
+    year, month, day = test_input
     try:
-        gregorian._check_range()
+        convert._check_gregorian_date(year, month, day)
     except OverflowError:
         pytest.fail()
 
@@ -130,41 +158,35 @@ def test_gregorian_valid_range(gregorian):
 )
 def test_gregorian_invalid_range(test_input, expected):
     with pytest.raises(OverflowError) as e:
-        convert.Gregorian(*test_input)._check_range()
+        convert._check_gregorian_date(*test_input)
     assert str(e.value) == expected
 
 
 @pytest.mark.parametrize(
     "test_input",
     [
-        (1410, 9, 30, "Lunar"),
-        (1356, 1, 1, "lunar"),
-        (1500, 12, 30, "lunar"),
-        (1315, 6, 23, "solar"),
-        (1456, 2, 25, "solar"),
+        (1410, 9, 30),
+        (1356, 1, 1),
+        (1500, 12, 30),
     ],
 )
 def test_hijri_valid_date(test_input):
-    year, month, day, calendar = test_input
+    year, month, day = test_input
     try:
-        convert._check_hijri_date(year, month, day, calendar)
-    except ValueError:
+        convert._check_hijri_date(year, month, day)
+    except (ValueError, OverflowError):
         pytest.fail()
 
 
 @pytest.mark.parametrize(
     "test_input, expected",
     [
-        ((1410, 8, 24, "other"), "calendar must be 'lunar' or 'solar'"),
-        ((37, 12, 30, "lunar"), "year must be in yyyy format"),
-        ((1410, 0, 1, "lunar"), "month must be in 1..12"),
-        ((1410, 13, 1, "lunar"), "month must be in 1..12"),
-        ((1410, 8, 30, "lunar"), "day must be in 1..29 for month"),
-        ((1368, 6, 30, "solar"), "day must be in 1..29 for month"),
-        ((1355, 12, 30, "lunar"), "date is out of range for conversion"),
-        ((1501, 1, 1, "lunar"), "date is out of range for conversion"),
-        ((1315, 6, 22, "solar"), "date is out of range for conversion"),
-        ((1456, 2, 26, "solar"), "date is out of range for conversion"),
+        ((37, 12, 30), "year must be in yyyy format"),
+        ((1410, 0, 1), "month must be in 1..12"),
+        ((1410, 13, 1), "month must be in 1..12"),
+        ((1410, 8, 30), "day must be in 1..29 for month"),
+        ((1355, 12, 30), "date is out of range for conversion"),
+        ((1501, 1, 1), "date is out of range for conversion"),
     ],
 )
 def test_hijri_invalid_date(test_input, expected):
@@ -174,14 +196,14 @@ def test_hijri_invalid_date(test_input, expected):
 
 
 def test_hijri_month_index():
-    assert convert._hijri_month_index(1410, 8, ummalqura.Lunar) == 656
+    assert convert._hijri_month_index(1410, 8) == 656
 
 
-def test_hijri2_month_days():
-    assert convert._hijri_month_days(656, ummalqura.Lunar) == 29
+def test_hijri_month_days():
+    assert convert._hijri_month_days(656) == 29
 
 
-def test_gregorian_to_julian():
+def test__gregorian_to_julian():
     assert convert._gregorian_to_julian(1990, 3, 26) == 2447977
 
 
