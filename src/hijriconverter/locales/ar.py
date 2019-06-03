0a1bd4cabf7e2dcac14670b75ad3d126acd44142
Mohammed Alshehri <542855+dralshehri@users.noreply.github.com>
2019-06-03 03:15:05 +0300

    refactor localization

---
+++ src/hijriconverter/locales/ar.py
@@ -0,0 +1,51 @@
+"""Localized Translation
+
+This module contains localized translation for week day names, month names,
+and notations.
+
+Note: months tuple must starts with empty string
+"""
+
+weekday_names = (
+    "الاثنين",
+    "الثلاثاء",
+    "الأربعاء",
+    "الخميس",
+    "الجمعة",
+    "السبت",
+    "الأحد",
+)
+
+gregorian_notation = "م"
+gregorian_months = (
+    "",
+    "يناير",
+    "فبراير",
+    "مارس",
+    "أبريل",
+    "مايو",
+    "يونيو",
+    "يوليو",
+    "أغسطس",
+    "سبتمبر",
+    "أكتوبر",
+    "نوفمبر",
+    "ديسمبر",
+)
+
+hijri_notation = "هـ"
+hijri_months = (
+    "",
+    "محرم",
+    "صفر",
+    "ربيع الأول",
+    "ربيع الثاني",
+    "جمادى الأولى",
+    "جمادى الآخرة",
+    "رجب",
+    "شعبان",
+    "رمضان",
+    "شوال",
+    "ذو القعدة",
+    "ذو الحجة",
+)
