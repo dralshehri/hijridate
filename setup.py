26b530650a9815f6817ff3f2fd6d72a0365e8dbb
dralshehri <dralshehri@users.noreply.github.com>
2019-06-03 05:04:22 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup, find_packages
 import pathlib
+from setuptools import setup, find_packages
 
 here = pathlib.Path(__file__).parent
 readme = (here / "README.rst").read_text(encoding="utf-8")
@@ -8,12 +8,13 @@ changelog = (here / "CHANGELOG.rst").read_text(encoding="utf-8")
 setup(
     name="hijriconverter",
     version="1.5.0",
-    description="Convert Hijri to/from Gregorian using Umm al-Qura calendar",
-    long_description=readme + "\n" + changelog,
-    url="",
+    description="Accurate Hijri-Gregorian date converter based on the "
+    "Umm al-Qura calendar",
+    long_description="\n".join([readme, changelog]),
+    long_description_content_type="text/x-rst",
+    url="https://github.com/dralshehri/hijri-converter",
     project_urls={
-        "Source Code": "https://github.com/dralshehri/hijri-converter",
-        "Documentation": "https://hijriconverter.readthedocs.io/en/latest",
+        "Documentation": "https://hijriconverter.readthedocs.io/en/latest"
     },
     author="Mohammed Alshehri",
     author_email="",
@@ -24,16 +25,15 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Internationalization",
         "Topic :: Software Development :: Localization",
         "Topic :: Utilities",
     ],
-    keywords="hijri date converter ummalqura saudi calendar",
+    keywords="hijri gregorian date converter ummalqura saudi calendar",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    python_requires=">=3.5",
+    python_requires=">=3.6",
 )
