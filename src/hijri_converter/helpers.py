9d4fbf36023cfbe06cb796c51d47c4ad59dc4949
Mohammed H Alshehri <542855+mhalshehri@users.noreply.github.com>
2021-09-04 09:41:44 +0300

    Refactor helper functions

--- src/hijri_converter/helpers.py
+++ src/hijri_converter/helpers.py
@@ -1,21 +1,41 @@
 """Helper methods for Hijri conversion."""
 
 
-def julian_to_ordinal(jd: int) -> int:
-    """Convert Julian Day (JD) number to ordinal number."""
-    return jd - 1721425
+def jdn_to_ordinal(jdn: int) -> int:
+    """Convert Julian day number (JDN) to date ordinal number.
+
+    :param jdn: Julian day number (JDN).
+    :type jdn: int
+    """
+
+    return jdn - 1721425
 
 
-def ordinal_to_julian(n: int) -> int:
-    """Convert ordinal number to Julian Day (JD) number."""
+def ordinal_to_jdn(n: int) -> int:
+    """Convert date ordinal number to Julian day number (JDN).
+
+    :param n: Date ordinal number.
+    :type n: int
+    """
+
     return n + 1721425
 
 
-def julian_to_reduced_julian(jd: int) -> int:
-    """Convert Julian Day (JD) number to Reduced Julian Day (RJD) number."""
-    return jd - 2400000
+def jdn_to_rjd(jdn: int) -> int:
+    """Return Reduced Julian Day (RJD) number from Julian day number (JDN).
+
+    :param jdn: Julian day number (JDN).
+    :type jdn: int
+    """
+
+    return jdn - 2400000
 
 
-def reduced_julian_to_julian(rjd: int) -> int:
-    """Convert Reduced Julian Day (RJD) number to Julian Day (JD) number."""
+def rjd_to_jdn(rjd: int) -> int:
+    """Return Julian day number (JDN) from Reduced Julian Day (RJD) number.
+
+    :param rjd: Reduced Julian Day (RJD) number.
+    :type rjd: int
+    """
+
     return rjd + 2400000
