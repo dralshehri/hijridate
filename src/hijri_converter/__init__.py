efc38c19b5b2dd3d3e7e591b1a215bec85451fad
Mohammed Alshehri <542855+dralshehri@users.noreply.github.com>
2021-09-23 02:50:00 +0300

    Fix format and typos

--- src/hijri_converter/__init__.py
+++ src/hijri_converter/__init__.py
@@ -1,8 +1,8 @@
-"""Accurate Hijri-Gregorian date converter based on the Umm al-Qura calendar.
+"""Accurate Hijri-Gregorian dates converter based on the Umm al-Qura calendar.
 
 https://github.com/dralshehri/hijri-converter
 """
 
-from hijri_converter.convert import Gregorian, Hijri
-
 __version__ = "2.2.1"
+
+from hijri_converter.convert import Gregorian, Hijri
