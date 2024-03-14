44bfa61a0b6882d9847cb7f2572dd782ea608250
Mohammed Alshehri <542855+dralshehri@users.noreply.github.com>
2024-03-14 18:22:16 +0300

    Improve documentation config

--- docs/_extensions/custom_sitemap.py
+++ docs/_extensions/custom_sitemap.py
@@ -1,30 +1,34 @@
-"""Custom sphinx extension to generate text file sitemap"""
+"""Custom sphinx extension to generate text file sitemap."""
 
 from pathlib import Path
 
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
+
 
 def setup(app):
-    """Setup connect events to the sitemap builder"""
+    """Setup connect events to the sitemap builder."""
     app.add_config_value("sitemap_excluded_pages", default=[], rebuild="")
     app.connect("html-page-context", add_links)
     app.connect("build-finished", create_sitemap)
     app.sitemap_pages = []
 
 
-def add_links(app, pagename, *args):
-    """As each page is built, collect page names for the sitemap"""
+def add_links(app, pagename):
+    """As each page is built, collect page names for the sitemap."""
     excluded_pages = app.config.sitemap_excluded_pages
-    excluded_pages = list(set(excluded_pages + ["404", "search", "genindex"]))
+    excluded_pages = list({*excluded_pages, "404", "search", "genindex"})
     if pagename not in excluded_pages and not pagename.startswith("_"):
         app.sitemap_pages.append(f"{pagename}")
 
 
 def create_sitemap(app, exception):
-    """Generates the sitemap.txt from the collected pages"""
+    """Generates the sitemap.txt from the collected pages."""
     site_url = app.builder.config.html_baseurl.rstrip("/") + "/"
 
     if not site_url or exception is not None or not app.sitemap_pages:
-        print("error while generating sitemap")
+        logger.error("error while generating sitemap")
         return
 
     output_path = Path(app.outdir)
@@ -37,6 +41,4 @@ def create_sitemap(app, exception):
     # update robots with sitemap url
     sitemap_url = site_url + sitemap_file.name
     robots_file = output_path.joinpath("robots.txt")
-    robots_file.write_text(
-        robots_file.read_text().replace("sitemap_url", sitemap_url)
-    )
+    robots_file.write_text(robots_file.read_text().replace("sitemap_url", sitemap_url))
--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# Full list of options can be found in the Sphinx documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+Full list of options can be found in the Sphinx documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 import re
 import sys
+
 from pathlib import Path
 
 # Set package name and path
@@ -13,7 +15,8 @@ package_path = Path("../src").joinpath(package_name).resolve()
 sys.path.append(str(package_path.parent))
 
 
-def read_version():
+def get_version():
+    """Return the current version of the package."""
     content = package_path.joinpath("__init__.py").read_text()
     pattern = re.compile(r"(?<=__version__\s=\s\").*(?=\")")
     return pattern.search(content).group()
@@ -28,7 +31,7 @@ sys.path.append(str(Path("_extensions").resolve()))
 project = "HijriDate"  # project name at PyPI and GitHub
 author = "Mohammed Alshehri"
 project_copyright = "2018 Mohammed Alshehri"
-version = read_version()
+version = get_version()
 
 #
 # -- General configuration -------------------------------------------------------------
@@ -111,11 +114,7 @@ html_theme_options = {
         {
             "name": "GitHub",
             "url": "https://github.com/dralshehri/hijridate",
-            "html": """
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                </svg>
-            """,
+            "html": '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>',  # noqa: E501
             "class": "",
         },
     ],
@@ -142,6 +141,4 @@ html_show_sphinx = False
 #
 # -- Options for manual pages output ---------------------------------------------------
 #
-man_pages = [
-    ("manpage", package_name, "convert Hijri-Gregorian dates", author, 7)
-]
+man_pages = [("manpage", package_name, "convert Hijri-Gregorian dates", author, 7)]
